a1ba8667273b5c418db7b73ecb0154d6a94f8cd8
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-02 17:48:47 -0800

    Change date to day

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
         default: Date.now,
     },
