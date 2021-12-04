f1e9db7918c8269b2ed705d4cb4e20840aaf2c81
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-03 16:58:34 -0800

    Format update

--- public/index.js
+++ public/index.js
@@ -1,13 +1,12 @@
 init();
 
 async function init() {
-  if (location.search.split("=")[1] === undefined) {
-    const workout = await API.getLastWorkout();
-    if (workout) {
-      location.search = "?id=" + workout._id;
-    } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
+    if (location.search.split("=")[1] === undefined) {
+        const workout = await API.getLastWorkout();
+        if (workout) {
+            location.search = "?id=" + workout._id;
+        } else {
+            document.querySelector("#continue-btn").classList.add("d-none");
+        }
     }
-  }
 }
-
